c0af1643a93176a1ce56abdbe0f87efc097ee3d1
Oleg Verych <olecom@gmail.com>
2017-09-07 14:28:56 +0300

    config: use 'process.env.REACT_APP_*'

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ export default {
     BUCKET: 'notes-app-uploads'
   },
   apiGateway: {
-    URL: 'https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod',
-    REGION: 'us-east-1',
+    URL: process.env.REACT_APP_TCM_API || 'https://localhost:3000/graphql',
+    REGION: 'eu-west-1',
   },
   cognito: {
-    REGION: 'us-east-1',
-    IDENTITY_POOL_ID: 'us-east-1:565cc505-5486-4278-bf0f-9736b9fdd8c5',
-    USER_POOL_ID : 'us-east-1_YEhdMFvix',
-    APP_CLIENT_ID : '29qpcqorti0ctjkur5h01dph5g',
+    REGION: 'eu-west-1',
+    IDENTITY_POOL_ID: 'us-east-1:000000', // is not used
+    USER_POOL_ID : process.env.REACT_APP_USER_POOL_ID || 'eu-west-1_00000000',
+    APP_CLIENT_ID : process.env.REACT_APP_APP_CLIENT_ID || '111111111111111111111111',
   }
 };
